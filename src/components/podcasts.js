334824e51b3e9a28844e12faef00ba219e487c7f
ahmetbicer <ahmtbicertr@gmail.com>
2020-07-12 20:21:16 +0300

    touchable opacity delayPressIn resetted to 0

--- src/components/podcasts.js
+++ src/components/podcasts.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, Dimensions, ScrollView} from 'react-native';
+import { View, Text, TouchableOpacity} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export function Podcast() {
     return (
       <View style={{padding:10, borderTopColor:"black",borderTopWidth:0.2}}>
+        <TouchableOpacity delayPressIn={0} onPress={() => console.log("hey")}>
           <View style={{flexDirection:"row", marginTop:10,marginLeft:10}}>
             <View style={{width:35, height:35, borderRadius:5, backgroundColor:"black"}} />
             <View style={{flexDirection:"column", marginTop:-2, marginLeft:6}}> 
@@ -22,13 +23,18 @@ export function Podcast() {
             </View>
           </View>
           <View style={{flexDirection:"row", marginTop:12,marginBottom:5, marginLeft:10, alignItems:"center"}}>
-            <View style={{borderRadius:99,borderWidth:0.3, padding:5, flexDirection:"row", justifyContent:"center",alignItems:"center"}}>
-            <MaterialCommunityIcons name="play-circle-outline" color={"#3a4eed"} size={24} />
-              <Text style={{marginLeft:5}}>1 sa. 36 dk.</Text>
-            </View>
-            <MaterialCommunityIcons name="playlist-plus" style={{marginLeft:15}} color={"#3a4eed"} size={28} />
-            <MaterialCommunityIcons name="arrow-down-circle-outline" style={{marginLeft:15}} color={"#3a4eed"} size={24} />
+            <TouchableOpacity delayPressIn={0} style={{borderRadius:99,borderWidth:0.3, padding:5, flexDirection:"row", justifyContent:"center",alignItems:"center"}}>
+              <MaterialCommunityIcons name="play-circle-outline" color={"#3a4eed"} size={24} />
+              <Text style={{marginHorizontal:5}}>1 sa. 36 dk.</Text>
+            </TouchableOpacity>
+            <TouchableOpacity delayPressIn={0} onPress={() => console.log("hey")}>
+              <MaterialCommunityIcons name="playlist-plus" style={{marginLeft:15}} color={"#3a4eed"} size={28} />
+            </TouchableOpacity>
+            <TouchableOpacity delayPressIn={0} onPress={() => console.log("hey")}>
+              <MaterialCommunityIcons name="arrow-down-circle-outline" style={{marginLeft:15}} color={"#3a4eed"} size={24} />
+            </TouchableOpacity>
           </View>
+          </TouchableOpacity>
       </View>
     );
   }
